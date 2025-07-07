5bae96a8b2feda7e9e40a896e9178034cfb87036
Omekagu Chukwuebuka <mikecheq5@gmail.com>
2025-07-07 15:33:41 +0100

    fix: Update Login component import path and create new Input component in Layouts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import Login from './admin/Login'
+import Login from './user/Login'
 
 export default function Home () {
   return (
--- src/pages/user/Login.jsx
+++ src/pages/user/Login.jsx
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router'
 import axios from 'axios'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import Input from '../../Components/Input'
+import Input from '../../Components/Layouts/Input'
 
 const Login = () => {
   const [email, setEmail] = useState('')
