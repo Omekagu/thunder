7e4ccd7af320d2b34610135fb833d62831b38f08
Omekagu Chukwuebuka <mikecheq5@gmail.com>
2025-07-08 11:30:34 +0100

    feat: Add BuyStatus, DepositStatus, EditInvestmentPlan, EmailTemplate, GeneralSetting, KycManagement, LoanApproval, ManageUsers, NotificationsPanel, RoleManagement, SellStatus, and WithdrawalStatus components

---
+++ src/pages/admin/BuyStatus.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function BuyStatus () {
+  return <div>BuyStatus</div>
+}
---
+++ src/pages/admin/GeneralSetting.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function GeneralSetting () {
+  return <div>GeneralSetting</div>
+}
