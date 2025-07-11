7e55ccf448edd39e5e6392eb9f4c2b97dde26ff0
Omekagu Chukwuebuka <mikecheq5@gmail.com>
2025-07-11 16:10:24 +0100

    popup notification page updated

--- src/pages/admin/PopUpPage.jsx
+++ src/pages/admin/PopUpPage.jsx
@@ -1,11 +1,67 @@
-import React from 'react'
+'use client'
+
+import { useEffect, useState } from 'react'
+import {
+  Snackbar,
+  Alert,
+  TextField,
+  Button,
+  Box,
+  Typography
+} from '@mui/material'
 import AdminHeader from '../../Components/Admin/AdminHeader'
 
 export default function PopUpPage () {
+  const [open, setOpen] = useState(false)
+  const [customMessage, setCustomMessage] = useState('')
+  const [submittedMessage, setSubmittedMessage] = useState(null)
+
+  const handleSend = () => {
+    if (customMessage.trim()) {
+      setSubmittedMessage(customMessage)
+      setOpen(true)
+      setCustomMessage('')
+    }
+  }
+
+  useEffect(() => {
+    if (submittedMessage) {
+      const timer = setTimeout(() => setOpen(false), 4000)
+      return () => clearTimeout(timer)
+    }
+  }, [submittedMessage])
+
   return (
-    <div>
+    <>
       <AdminHeader />
-      PopUpPage
-    </div>
+      <Box>
+        <Typography variant='h6' fontWeight={600} mb={2}>
+          Send User Notification
+        </Typography>
+        <Box display='flex' gap={2} flexDirection={{ xs: 'column', sm: 'row' }}>
+          <TextField
+            label='Notification Message'
+            variant='outlined'
+            value={customMessage}
+            onChange={e => setCustomMessage(e.target.value)}
+            fullWidth
+          />
+          <Button variant='contained' onClick={handleSend}>
+            Send
+          </Button>
+        </Box>
+
+        <Snackbar
+          open={open}
+          onClose={() => setOpen(false)}
+          anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+          autoHideDuration={40000}
+        >
+          <Alert severity='success' variant='outlined'>
+            {submittedMessage}
+          </Alert>
+        </Snackbar>
+      </Box>
+    </>
   )
 }
--- src/pages/user/UserDashboard.jsx
+++ src/pages/user/UserDashboard.jsx
@@ -2,13 +2,18 @@ import UserAmount from '../../Components/User/UserAmount'
 import InvestmentHistory from '../../Components/User/InvestmentHistory'
 import MainHeader from '../../Components/User/MainHeader'
 import PrivateRoute from './PrivateRoute'
+import UserNotificationPopup from '../../Components/Admin/UserNotificationPopup'
 
 export default function UserDashboard () {
   return (
     <PrivateRoute>
       <div className='dashboard-container'>
         <MainHeader />
-
+        <UserNotificationPopup
+          message='Welcome back!'
+          type='success'
+          openInitially={true}
+        />
         {/* TOP CARD from Image 2 */}
         <UserAmount />
 
